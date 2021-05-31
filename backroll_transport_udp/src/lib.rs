861e69938ebc04ef47ad90e6f984583a1778f1ab
james7132 <contact@jamessliu.com>
2021-05-31 09:00:32 -0700

    Add basic connection tests for backroll-transport-udp

--- backroll_transport_udp/src/lib.rs
+++ backroll_transport_udp/src/lib.rs
@@ -9,7 +9,12 @@ use std::time::{Duration, Instant};
 use tracing::{debug, error};
 
 const CLEANUP_INTERVAL: Duration = Duration::from_millis(1000);
-pub const MAX_TRANSMISSION_UNIT: usize = 1450;
+
+/// The maximum size of packet that can be sent or recieved, in bytes.
+///
+/// This is based on the ethernet standard MTU (1500 bytes), the size of the
+/// IPv4/6 header (20/40 bytes), and the UDP header (8 bytes).
+pub const MAX_TRANSMISSION_UNIT: usize = 1452;
 
 pub struct UdpConnectionConfig {
     pub addr: SocketAddr,
@@ -17,6 +22,9 @@ pub struct UdpConnectionConfig {
 }
 
 impl UdpConnectionConfig {
+    /// Shorthand for creating unbounded connections. Unbounded connections
+    /// will never drop a recieved packet. However, because it will not drop
+    /// packets, malicious actors can flood the connection.
     pub fn unbounded(addr: SocketAddr) -> UdpConnectionConfig {
         Self {
             addr,
@@ -24,6 +32,8 @@ impl UdpConnectionConfig {
         }
     }
 
+    /// Shorthand for creating bounded connections. Bounded connections
+    /// will drop a recieved packet if the recieve queue is full.
     pub fn bounded(addr: SocketAddr, limit: usize) -> UdpConnectionConfig {
         Self {
             addr,
@@ -39,7 +49,13 @@ pub struct UdpManager {
 }
 
 impl UdpManager {
-    /// Binds a `[UdpSocket]` and starts listening on it.
+    /// Binds a [UdpSocket] and starts listening on it.
+    ///
+    /// # Errors
+    /// Returns a [std::io::Error] if it fails to bind to the provided socket addresses
+    /// or start an async poll on the socket.
+    ///
+    /// [UdpSocket]: async_net::UdpSocket
     pub fn bind(pool: TaskPool, addrs: impl ToSocketAddrs) -> std::io::Result<Self> {
         let blocking = BlockingUdpSocket::bind(addrs)?;
         let socket = UdpSocket::try_from(blocking)?;
@@ -56,10 +72,13 @@ impl UdpManager {
         Ok(manager)
     }
 
-    /// Creates a `[Peer]` bound to a specific target `[SocketAddr]`.
+    /// Creates a [Peer] bound to a specific target [SocketAddr].
     ///
     /// Note this does not block or send any I/O. It simply creates the
     /// tasks for reading and sending.
+    ///
+    /// [Peer]: backroll_transport::Peer
+    /// [SocketAddr]: std::net::SocketAddr
     pub fn connect(&self, config: UdpConnectionConfig) -> Peer {
         let peer = if let Some(limit) = config.max_queue_size {
             self.peers.create_bounded(config.addr, limit)
@@ -73,7 +92,7 @@ impl UdpManager {
         peer
     }
 
-    /// Disconnects the connection to a given `[SocketAddr]` if available.
+    /// Disconnects the connection to a given [SocketAddr] if available.
     ///
     /// [SocketAddr]: std::net::SocketAddr
     pub fn disconnect(&self, addr: SocketAddr) {
@@ -127,5 +146,82 @@ impl UdpManager {
                 debug!("Dropped packet for disconnected packet queue: {} ", addr);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    #[serial_test::serial]
+    pub fn test_basic_connect() {
+        const ADDR_A: &str = "127.0.0.1:1000";
+        const ADDR_B: &str = "127.0.0.1:1001";
+        let pool = TaskPool::new();
+
+        let socket_a = UdpManager::bind(pool.clone(), ADDR_A).unwrap();
+        let socket_b = UdpManager::bind(pool.clone(), ADDR_B).unwrap();
+
+        let peer_a = socket_b.connect(UdpConnectionConfig::unbounded(ADDR_A.parse().unwrap()));
+        let peer_b = socket_a.connect(UdpConnectionConfig::unbounded(ADDR_B.parse().unwrap()));
+
+        let msg_a: Box<[u8]> = b"Hello A!"[0..].into();
+        let msg_b: Box<[u8]> = b"Hello B!"[0..].into();
+
+        peer_a.try_send(msg_b.clone()).unwrap();
+        peer_b.try_send(msg_a.clone()).unwrap();
+
+        let recv_msg_a = futures::executor::block_on(peer_a.recv()).unwrap();
+        let recv_msg_b = futures::executor::block_on(peer_b.recv()).unwrap();
+
+        assert_eq!(msg_a, recv_msg_a);
+        assert_eq!(msg_b, recv_msg_b);
+    }
+
+    #[test]
+    #[serial_test::serial]
+    pub fn test_multiple_send() {
+        const ADDR_A: &str = "127.0.0.1:1000";
+        const ADDR_B: &str = "127.0.0.1:1001";
+        let pool = TaskPool::new();
+
+        let socket_a = UdpManager::bind(pool.clone(), ADDR_A).unwrap();
+        let socket_b = UdpManager::bind(pool.clone(), ADDR_B).unwrap();
+
+        let peer_a = socket_b.connect(UdpConnectionConfig::unbounded(ADDR_A.parse().unwrap()));
+        let peer_b = socket_a.connect(UdpConnectionConfig::unbounded(ADDR_B.parse().unwrap()));
+
+        peer_a.try_send(b"100"[0..].into()).unwrap();
+        peer_a.try_send(b"101"[0..].into()).unwrap();
+        peer_a.try_send(b"102"[0..].into()).unwrap();
+        peer_a.try_send(b"103"[0..].into()).unwrap();
+        peer_a.try_send(b"104"[0..].into()).unwrap();
+        peer_a.try_send(b"105"[0..].into()).unwrap();
+
+        assert_eq!(
+            futures::executor::block_on(peer_b.recv()),
+            Ok(b"100"[0..].into())
+        );
+        assert_eq!(
+            futures::executor::block_on(peer_b.recv()),
+            Ok(b"101"[0..].into())
+        );
+        assert_eq!(
+            futures::executor::block_on(peer_b.recv()),
+            Ok(b"102"[0..].into())
+        );
+        assert_eq!(
+            futures::executor::block_on(peer_b.recv()),
+            Ok(b"103"[0..].into())
+        );
+        assert_eq!(
+            futures::executor::block_on(peer_b.recv()),
+            Ok(b"104"[0..].into())
+        );
+        assert_eq!(
+            futures::executor::block_on(peer_b.recv()),
+            Ok(b"105"[0..].into())
+        );
     }
 }

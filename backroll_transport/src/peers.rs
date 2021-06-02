ee3fe65d1df8f439eed3f38af4e4848585b78144
james7132 <contact@jamessliu.com>
2021-06-02 16:19:43 -0700

    Fix flush_disconnected to properly use retain

--- backroll_transport/src/peers.rs
+++ backroll_transport/src/peers.rs
@@ -77,14 +77,7 @@ impl<T: Eq + Hash> Peers<T> {
 
     /// Removes all peers that are disconnected.
     pub fn flush_disconnected(&self) {
-        self.0.retain(|_, peer| {
-            if !peer.is_connected() {
-                peer.disconnect();
-                true
-            } else {
-                false
-            }
-        })
+        self.0.retain(|_, peer| peer.is_connected())
     }
 }
 

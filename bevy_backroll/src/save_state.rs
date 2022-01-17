8bb8d9465b308c2d051b59a7c737674867c0e458
james7132 <contact@jamessliu.com>
2022-01-17 01:32:01 -0800

    Handle missing resources or components

--- bevy_backroll/src/save_state.rs
+++ bevy_backroll/src/save_state.rs
@@ -1,18 +1,16 @@
 use bevy_ecs::prelude::*;
-use bevy_log::*;
+use parking_lot::Mutex;
 use std::any::*;
 use std::collections::HashMap;
 use std::sync::Arc;
-use parking_lot::Mutex;
 
 #[derive(Clone)]
-struct ComponentSlab<T: Clone> {
-    components: Vec<T>,
-    entities: Vec<Entity>,
+struct SavedComponents<T: Clone> {
+    components: HashMap<Entity, T>,
 }
 
 /// A mutable builder for [`SaveState`]s.
-pub struct SaveStateBuilder {
+pub(crate) struct SaveStateBuilder {
     state: Mutex<HashMap<TypeId, Box<dyn Any + Send + Sync>>>,
 }
 
@@ -21,33 +19,6 @@ impl SaveStateBuilder {
         Self {
             state: Mutex::new(HashMap::new()),
         }
-    }
-
-    pub(crate) fn save_resource<T: Clone + Send + Sync + 'static>(&self, resource: Res<T>) {
-        self.state
-	.lock()
-            .insert(TypeId::of::<T>(), Box::new(resource.clone()));
-    }
-
-    pub(crate) fn save_components<T: Component + Clone>(&self, query: Query<(Entity, &T)>) {
-	let iter = query.iter();
-	let (_, max) = iter.size_hint();
-        let (mut components, mut entities) = if let Some(max) = max {
-		(Vec::with_capacity(max), Vec::with_capacity(max))
-	} else {
-		(Vec::new(), Vec::new())
-	};
-        for (entity, component) in iter {
-            entities.push(entity);
-            components.push(component.clone());
-        }
-        self.state.lock().insert(
-            TypeId::of::<ComponentSlab<T>>(),
-            Box::new(ComponentSlab {
-                components,
-                entities,
-            }),
-        );
     }
 
     pub fn build(self) -> SaveState {
@@ -59,66 +30,84 @@ impl SaveStateBuilder {
 #[derive(Clone)]
 pub struct SaveState(Arc<HashMap<TypeId, Box<dyn Any + Send + Sync>>>);
 
-impl SaveState {
-    pub(crate) fn load_resource<T: Clone + Send + Sync + 'static>(&self, mut resource: ResMut<T>) {
-        // HACK: This is REALLY going to screw with any change detection on these types.
-        let saved = self
-            .0
-            .get(&TypeId::of::<T>())
-            .unwrap()
-            .downcast_ref::<T>()
-            .unwrap();
-        *resource = saved.clone();
-    }
-
-    pub(crate) fn load_components<T: Component + Clone>(&self, mut query: Query<&mut T>) {
-        let slab = self
-            .0
-            .get(&TypeId::of::<ComponentSlab<T>>())
-            .unwrap()
-            .downcast_ref::<ComponentSlab<T>>()
-            .unwrap();
-
-        // HACK: This is REALLY going to screw with any change detection on these types.
-        for (entity, component) in slab.entities.iter().zip(slab.components.iter()) {
-            if let Ok(mut comp) = query.get_mut(*entity) {
-                *comp = component.clone();
-            } else {
-                warn!(
-                    "Attempted to load component '{}' from a save state for entity {:?} but the entity no longer exists.",
-                    type_name::<T>(),
-                    entity
-                )
-            }
-        }
-    }
-}
-
 pub(crate) fn save_resource<T: Clone + Send + Sync + 'static>(
     save_state: Res<SaveStateBuilder>,
-    resource: Res<T>,
+    resource: Option<Res<T>>,
 ) {
-    save_state.save_resource(resource);
+    if let Some(resource) = resource {
+        save_state
+            .state
+            .lock()
+            .insert(TypeId::of::<T>(), Box::new(resource.clone()));
+    }
 }
 
 pub(crate) fn load_resource<T: Clone + Send + Sync + 'static>(
     save_state: Res<SaveState>,
-    resource: ResMut<T>,
+    resource: Option<ResMut<T>>,
+    mut commands: Commands,
 ) {
-    save_state.load_resource(resource);
+    // HACK: This is REALLY going to screw with any change detection on these types.
+    let saved = save_state.0.get(&TypeId::of::<T>());
+    match (saved, resource) {
+        (Some(saved), Some(mut resource)) => {
+            let saved = saved.downcast_ref::<T>().unwrap();
+            *resource = saved.clone();
+        }
+        (Some(saved), None) => {
+            let saved = saved.downcast_ref::<T>().unwrap();
+            commands.insert_resource(saved.clone());
+        }
+        (None, Some(_)) => {
+            commands.remove_resource::<T>();
+        }
+        (None, None) => {}
+    }
 }
 
-// This disallows parallelization while saving. Is this design OK?
 pub(crate) fn save_components<T: Component + Clone>(
     save_state: Res<SaveStateBuilder>,
     query: Query<(Entity, &T)>,
 ) {
-    save_state.save_components(query);
+    let components: HashMap<Entity, T> = query
+        .iter()
+        .map(|(entity, component)| (entity, component.clone()))
+        .collect();
+    if !components.is_empty() {
+        save_state.state.lock().insert(
+            TypeId::of::<SavedComponents<T>>(),
+            Box::new(SavedComponents { components }),
+        );
+    }
 }
 
 pub(crate) fn load_components<T: Component + Clone>(
     save_state: Res<SaveState>,
-    query: Query<&mut T>,
+    mut query: Query<(Entity, &mut T)>,
+    mut commands: Commands,
 ) {
-    save_state.load_components(query);
+    let saved = save_state.0.get(&TypeId::of::<SavedComponents<T>>());
+    let slab = if let Some(slab) = saved {
+        slab.downcast_ref::<SavedComponents<T>>().unwrap()
+    } else {
+        for (entity, _) in query.iter() {
+            commands.entity(entity).remove::<T>();
+        }
+        return;
+    };
+
+    let mut components = slab.components.clone();
+    // HACK: This is REALLY going to screw with any change detection on these types.
+    for (entity, mut comp) in query.iter_mut() {
+        if let Some(component) = components.remove(&entity) {
+            *comp = component;
+        } else {
+            commands.entity(entity).remove::<T>();
+        }
+    }
+
+    // Everything else that remains was either removed or had it's entity despawned.
+    for (entity, component) in components {
+        commands.entity(entity).insert(component);
+    }
 }
